05ef56ea8e919f724caf5e25382288b42b2f7a0e
Ned <67144261+Ne-d@users.noreply.github.com>
2025-03-25 16:46:33 +0100

    Give Homme. his nametag back.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,24 +1,26 @@
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent m_agent;
-    [SerializeField] private GameObject m_mesh;
-    
+    [SerializeField] private GameObject m_visual;
+    private Camera m_camera;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        m_camera = Camera.main;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        m_agent.SetDestination(Camera.main.transform.position);
+        m_agent.SetDestination(m_camera.transform.position);
         
-        m_mesh.transform.LookAt(Camera.main.transform.position);
-        m_mesh.transform.rotation = Quaternion.Euler(0, m_mesh.transform.rotation.eulerAngles.y, 0);
+        m_visual.transform.LookAt(m_camera.transform.position);
+        m_visual.transform.rotation = Quaternion.Euler(0, m_visual.transform.rotation.eulerAngles.y, 0);
     }
 }
