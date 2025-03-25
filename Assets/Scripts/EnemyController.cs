42d2d64dd04a47354d08f8d6553aefe5d7a6868f
Ned <67144261+Ne-d@users.noreply.github.com>
2025-03-25 16:05:56 +0100

    Fix enemy orientation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent m_agent;
+    [SerializeField] private GameObject m_mesh;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,5 +17,8 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
         m_agent.SetDestination(Camera.main.transform.position);
+        
+        m_mesh.transform.LookAt(Camera.main.transform.position);
+        m_mesh.transform.rotation = Quaternion.Euler(0, m_mesh.transform.rotation.eulerAngles.y, 0);
     }
 }
