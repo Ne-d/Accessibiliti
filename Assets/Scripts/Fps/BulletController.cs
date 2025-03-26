e62e7590f38ab124e45063ee424dc20f35cdd02d
Ned <67144261+Ne-d@users.noreply.github.com>
2025-03-26 11:45:29 +0100

    Add timeout to bullets

--- Assets/Scripts/Fps/BulletController.cs
+++ Assets/Scripts/Fps/BulletController.cs
@@ -1,16 +1,27 @@
+using System;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
     [SerializeField] private float m_speed;
     [SerializeField] private float m_damage;
+    [SerializeField] private float m_lifeTime;
 
     private bool m_shouldDie;
+    private float m_spawnTime;
+
+    private void Start()
+    {
+        m_spawnTime = Time.time;
+    }
 
     // Update is called once per frame
     private void Update()
     {
         transform.Translate(Vector3.forward * (m_speed * Time.deltaTime));
+
+        if (Time.time > m_spawnTime + m_lifeTime)
+            m_shouldDie = true;
 
         if (m_shouldDie)
             Die();
